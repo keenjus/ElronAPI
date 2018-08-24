00f68b07833927a2ae14a4d3b6757a3c72d79ce4
Martin <martinpetoffer@gmail.com>
2018-08-25 01:26:44 +0300

    Timezone id's differ on linux and windows

--- ElronAPI.Domain/Helpers/DateTimeHelper.cs
+++ ElronAPI.Domain/Helpers/DateTimeHelper.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Linq;
 
 namespace ElronAPI.Domain.Helpers
 {
     public static class DateTimeHelper
     {
+        private static string _estonianTimeZoneId;
+
+        private static string EstonianTimeZoneId
+        {
+            get
+            {
+                if (_estonianTimeZoneId != null) return _estonianTimeZoneId;
+
+                var timezones = TimeZoneInfo.GetSystemTimeZones()
+                    .Where(x => x.Id == "Europe/Tallinn" || x.Id == "E. Europe Standard Time");
+
+                _estonianTimeZoneId = timezones.First().Id;
+                return _estonianTimeZoneId;
+            }
+        }
+
         public static DateTime NowEstonian()
         {
-            return TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("E. Europe Standard Time"));
+            return TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById(EstonianTimeZoneId));
         }
 
         public static TimeSpan TimeUntilMidnight(DateTime dateTime)

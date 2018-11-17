7b9b9b4ed45676e497fcb04ce48b0a05b009776e
Martin <martinpetoffer@gmail.com>
2018-11-17 16:30:02 +0200

    Added logging to privacy policy page

--- ElronAPI.Api/Controllers/PrivacyPolicyController.cs
+++ ElronAPI.Api/Controllers/PrivacyPolicyController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace ElronAPI.Api.Controllers
 {
     [Route("privacy")]
     public class PrivacyPolicyController : Controller
     {
+        private readonly ILogger<PrivacyPolicyController> _logger;
+
+        public PrivacyPolicyController(ILogger<PrivacyPolicyController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
+            _logger.LogInformation("Someone visited the privacy policy page!");
             return View();
         }
     }
